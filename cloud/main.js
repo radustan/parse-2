899846812086836607fdbf6242cafd861f818458
radustan <raduadrian.stan@gmail.com>
2016-03-23 12:24:21 +0200

    Added cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,45 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('getActiveProducts', function(req, res){
+  var query = new Parse.Query("Products");
+  query.notEqualTo('isDeleted', 1);
+  query.find({
+    success: function(results) {
+      res.success(results)
+    },
+    error: function() {
+      res.error("Could not retrieve users");
+    }
+  })
+});
+
+Parse.Cloud.define('deleteProduct', function(req, res){
+  var query = new Parse.Query("Products");
+  query.equalTo('objectId', req.params.id);
+  query.first({
+    success: function(result) {
+        if (result) {
+            if (result.get('createdby').id == req.params.createdBy) {
+              result.set('isDeleted', 1);
+              result.save(null, {
+                success: function(){
+                  res.success('Product Deleted');
+                },
+                error: function(status) {
+                  res.error('Not saved');
+                }
+              });
+            } else {
+               res.error("Not allowed");
+            }
+        } else {
+            res.error("Could not find product");
+        }
+    },
+    error: function() {
+      res.error("Could not retrieve product");
+    }
+  })
+});
--- index.js
+++ index.js
@@ -12,10 +12,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://127.0.0.1:27017/parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'APPLICATION_ID',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
